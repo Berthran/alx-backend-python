5bedc23faa3057d7928653737304110e8b8c22b6
Daniel Berthran <berthlearn@gmail.com>
2024-10-11 03:25:38 +0100

    correct variable name

--- 1-concat.py
+++ 1-concat.py
@@ -4,8 +4,8 @@ Type-annotated function
 '''
 
 
-def concat(str1: str, b: str) -> str:
+def concat(str1: str, str2: str) -> str:
     '''
     returns a concatenated string of str1 and str2
     '''
-    return a + b
+    return str1 + str2
