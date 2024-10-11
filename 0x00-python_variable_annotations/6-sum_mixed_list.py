67e8bf53fc83ce68db337c04c5565da24f4938ec
Daniel Berthran <berthlearn@gmail.com>
2024-10-11 03:45:29 +0100

    correct argument name in return statement

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -11,4 +11,4 @@ def sum_mixed_list(mxd_list: List[int | float]) -> float:
     '''
     returns sum of list elements as float
     '''
-    return float(sum(input_list))
+    return float(sum(mxd_list))
