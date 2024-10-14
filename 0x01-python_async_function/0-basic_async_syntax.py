81b7d9706a41e63923ae891a4f8257b5a0c11dcc
Daniel Berthran <berthlearn@gmail.com>
2024-10-14 16:51:24 +0100

    add type annotation to function using Awaitable

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,14 +6,14 @@ Create a simple asyncronous in Python
 
 import random
 import asyncio
+from typings import Awaitable
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int=10) -> Awaitable[float]:
     '''
     waits for a random delay between 0 and max_delay
     seconds and eventually returns it
     '''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    print('wait_random delayed for {} secs'.format(delay))
     return (delay)
