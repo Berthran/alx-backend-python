5a1c8180c6587a37eebc91d57fe642afd504006e
Daniel Berthran <berthlearn@gmail.com>
2024-10-15 12:07:09 +0100

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+an async generator with no arguments
+'''
+
+
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''
+    Yields a random floating point number between 0 and 10
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        num = random.uniform(0, 10)
+        yield num
